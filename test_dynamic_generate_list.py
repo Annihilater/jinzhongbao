b681d82bc2a65f3cfcbe20baeab5f4cc94dbbdc7
Annihilater <yanmiexingkong@gmail.com>
2019-02-03 12:16:37 +0800

    test:使用 globals() 根据对象的名称动态获取该对象

---
+++ test_dynamic_generate_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date  : 2019/2/3 09:51
+# @Author: PythonVampire
+# @email : vampire@ivamp.cn
+# @File  : test_dynamic_generate_list.py
+
+
+# for i in range(4):
+#     name = 'v' + str(i)
+#     locals()['v' + str(i)] = i
+month = 201808
+
+str_month = 'month'
+a = globals()[str_month]
+
+L = list(str_month)
+print(L)
