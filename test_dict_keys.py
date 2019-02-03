89a839e5067db510ad49a73b1beb4955e93846ad
Annihilater <yanmiexingkong@gmail.com>
2019-02-03 12:14:15 +0800

    test:获取字典的键

---
+++ test_dict_keys.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date  : 2019/2/3 09:07
+# @Author: PythonVampire
+# @email : vampire@ivamp.cn
+# @File  : test_dict_keys.py
+
+d = {'a': 1}
+key = list(d.keys())[0]
+print(key)
