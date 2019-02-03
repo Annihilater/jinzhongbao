fe942a060913bc46605974dcfe50e065b365812d
Annihilater <yanmiexingkong@gmail.com>
2019-02-03 12:08:37 +0800

    test:url解码时写的测试代码

--- test8.py
+++ test8.py
@@ -4,5 +4,14 @@
 # @Author: PythonVampire
 # @email : vampire@ivamp.cn
 # @File  : test8.py
+import json
+from urllib.parse import quote
+from config import encode_str, dict_record
 
 
+json_record = json.dumps(dict_record).upper()
+print(json_record)
+encode_record = quote(json_record.encode('gb2312'))
+encode_record = encode_record.replace('%20', '')
+print(encode_str)
+print(encode_record)
