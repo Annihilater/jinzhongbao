39c8ed8366b938522ecbfa89968802bbe9f56056
Annihilater <yanmiexingkong@gmail.com>
2019-02-04 15:21:12 +0800

    test: 使用 selenium 模拟鼠标点击操作到达小票图片页面获取页面 url，但是仅仅只能获得 10 条记录

--- test/jinzhongbao.py
+++ test/jinzhongbao.py
@@ -7,6 +7,8 @@
 # 本文件没有写完，因为写到中途发现了更好的方法所以放弃了
 
 import re
+import time
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
@@ -14,44 +16,86 @@ import config
 
 
 class Spider:
-    url = config.url
-    fake_url = config.fake_url
-    User_Agent = config.User_Agent
-    language = config.language
-    cookies = config.cookies
-    chrome_driver = config.chromedriver_path
+    def __init__(self):
+        self.url = config.url
+        self.fake_url = config.fake_url
+        self.User_Agent = config.User_Agent
+        self.language = config.language
+        self.cookies = config.cookies
+        self.chrome_driver = config.chromedriver_path
 
-    root_pattern = config.root_pattern
-    merchant_order_name_pattern = config.merchant_order_name_pattern
-    amount_pattern = config.amount_pattern
+        self.root_pattern = config.root_pattern
+        self.merchant_order_name_pattern = config.merchant_order_name_pattern
+        self.amount_pattern = config.amount_pattern
+
+        self.picture_url = []
+        self.picture_single_url_dict = {}
 
     def go(self):
-        html = self.__fetch__content()
-        html = self.__analysis(html)
-        with open('content.html', 'w') as f:
-            f.write(str(html))
+        self.__fetch__content()
+        # html = self.__analysis(html)
+        # with open('content.html', 'w') as f:
+        #     f.write(str(html))
 
     def __fetch__content(self):
         opts = Options()
         opts.add_argument("user-agent={}".format(self.User_Agent))
         opts.add_argument("language={}".format(self.language))
 
-        browser = webdriver.Chrome(self.chrome_driver, chrome_options=opts)
-        browser.set_window_size(500, 900)
-        browser.set_window_position(1300, 10)
-        # new_user_agent = browser.execute_script("return navigator.userAgent;")
-        # new_language = browser.execute_script("return navigator.language;")
+        driver = webdriver.Chrome(self.chrome_driver, chrome_options=opts)
+        # driver.set_window_size(500, 900)
+        # driver.set_window_position(1300, 10)
+        # new_user_agent = driver.execute_script("return navigator.userAgent;")
+        # new_language = driver.execute_script("return navigator.language;")
         # print(new_user_agent)
         # print(new_language)
-        browser.get(self.fake_url)
-        print('current_url:', browser.current_url)
+        driver.get(self.fake_url)
+        print('current_url:', driver.current_url)
 
         for cookie in self.cookies:
-            browser.add_cookie(cookie_dict=cookie)
+            driver.add_cookie(cookie_dict=cookie)
 
-        browser.get(self.url)
-        page_source = browser.page_source
-        return page_source
+        try:
+            for i in range(2, 12):
+                driver.get(self.url)
+
+                trade_time_xpath = '//*[@id="container"]/div/div[2]/div[3]/div[' + str(i) + ']/div/a/div/p[5]'
+                trade_detail_xpath = '//*[@id="container"]/div/div[2]/div[3]/div[' + str(i) + ']/div'
+                trade_time = driver.find_element_by_xpath(trade_time_xpath).text
+                driver.find_element_by_xpath(trade_detail_xpath).click()
+                # time.sleep(1)
+                driver.implicitly_wait(1)
+
+                driver.find_element_by_xpath('//*[@id="dzpd"]/a').click()
+                picture_url = driver.current_url
+                self.picture_single_url_dict[trade_time] = picture_url
+                self.picture_url.append(self.picture_single_url_dict)
+                self.picture_single_url_dict = {}
+
+            # driver.get(self.url)
+            # trade_info_list = driver.find_elements_by_css_selector('.weui-cell__bd')
+            # for trade_info_web_element in trade_info_list:
+            #     trade_info = trade_info_web_element.text
+            #     trade_time = trade_info.split('\n')[4]
+            #
+            #     driver.find_element_by_class_name('weui-cell').click()
+            #     driver.implicitly_wait(1)
+            #
+            #     driver.find_element_by_xpath('//*[@id="dzpd"]/a').click()
+            #
+            #     picture_url = driver.current_url
+            #     self.picture_single_url_dict[trade_time] = picture_url
+            #     self.picture_url.append(self.picture_single_url_dict)
+            #     self.picture_single_url_dict = {}
+            #     driver.back()
+            #
+            # time.sleep(5)
+            # driver.close()
+            a = 1
+        except Exception as e:
+            print(e)
+        finally:
+            driver.close()
 
     def __analysis(self, html):
         root_html = re.findall(self.root_pattern, html, re.S)
