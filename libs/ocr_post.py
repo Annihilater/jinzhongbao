cc1b9db91c4ef90069dd09b4cc83a84015ed0dae
Annihilater <yanmiexingkong@gmail.com>
2019-02-23 15:41:33 +0800

    feat: 调用阿里云图片识别 api

--- libs/ocr_post.py
+++ libs/ocr_post.py
@@ -4,5 +4,22 @@
 # @Author: PythonVampire
 # @email : vampire@ivamp.cn
 # @File  : ocr_post.py
+import urllib.request
+import urllib.parse
+import json
+import time
 
 
+# 调用阿里云 ocr 服务发送 post 请求
+def post_url(url, img_dict, headers):
+    try:
+        params = json.dumps(img_dict).encode(encoding='UTF8')
+        req = urllib.request.Request(url, params, headers)
+        r = urllib.request.urlopen(req)
+        html = r.read()
+        r.close()
+        return html.decode("utf8")
+    except urllib.error.HTTPError as e:
+        print(e.code)
+        print(e.read().decode("utf8"))
+    time.sleep(1)
