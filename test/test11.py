7fdaf2636048ff6bcfa9aad7c5a3fe436e54a2c6
Annihilater <yanmiexingkong@gmail.com>
2019-03-08 11:15:55 +0800

    test: datetime.datetime.strptime(text, %Y%m)

---
+++ test/test11.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date  : 2019/3/5 13:14
+# @Author: PythonVampire
+# @email : vampire@ivamp.cn
+# @File  : test11.py
+import datetime
+
+text = '201901'
+y = datetime.datetime.strptime(text, '%Y%m')
+print(y)
+print(type(y))
