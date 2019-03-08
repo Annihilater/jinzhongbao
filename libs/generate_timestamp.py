8285e97c55927cb4ad4eb200a5648e9d9bfa66d4
Annihilater <yanmiexingkong@gmail.com>
2019-03-08 11:07:47 +0800

    feat: generate current timestamp

---
+++ libs/generate_timestamp.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date  : 2019/3/1 16:21
+# @Author: PythonVampire
+# @email : vampire@ivamp.cn
+# @File  : generate_timestamp.py
+import time
+
+
+def generate_timestamp():
+    t = time.time()
+    t = int(round(t * 1000))
+    return t
+
+
+if __name__ == '__main__':
+    timestamp = generate_timestamp()
+    print(timestamp)
