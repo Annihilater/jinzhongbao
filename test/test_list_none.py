9aa6f816128293c6c92856d9a129e2912a83088c
Annihilater <yanmiexingkong@gmail.com>
2019-03-08 11:20:06 +0800

    test: test empty list in for cycle

---
+++ test/test_list_none.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date  : 2019/3/2 18:34
+# @Author: PythonVampire
+# @email : vampire@ivamp.cn
+# @File  : test_list_none.py
+
+
+l = []
+for item in l:
+    print(item)
