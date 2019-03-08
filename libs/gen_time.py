810956c18499770028cf766cbcb74cae4ff630db
Annihilater <yanmiexingkong@gmail.com>
2019-03-08 11:05:23 +0800

    feat: generate current time (datetime)

---
+++ libs/gen_time.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date  : 2019/3/2 22:05
+# @Author: PythonVampire
+# @email : vampire@ivamp.cn
+# @File  : gen_time.py
+import datetime
+
+
+def gen_current_time():
+    now = datetime.datetime.now()
+    return now
+
+
+if __name__ == '__main__':
+    t = gen_current_time()
+    year = t.year
+    month = t.month
+    print(t)
+    print(year)
+    print(month)
