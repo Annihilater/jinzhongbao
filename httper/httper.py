116736a0f33c4fc4e390bc2570f747b04a0e63d4
Annihilater <yanmiexingkong@gmail.com>
2019-03-08 11:26:43 +0800

    feat: add HTTP class to make http session for request

---
+++ httper/httper.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date  : 2019/3/1 16:10
+# @Author: PythonVampire
+# @email : vampire@ivamp.cn
+# @File  : httper.py
+import requests
+
+from config import USER_AGENT, CONTENT_TYPE, REQUESTS_COOKIES, LANGUAGE, COOKIE, HTTP_HEADERS
+from libs import cookie_to_dict
+
+
+class HTTP:
+    user_agent = USER_AGENT
+    language = LANGUAGE
+    content_type = CONTENT_TYPE
+    cookies = cookie_to_dict(REQUESTS_COOKIES)
+    cookie = COOKIE
+
+    @classmethod
+    def make_session(cls):
+        session = requests.session()
+        session.headers.update({"User-Agent": cls.user_agent, 'Content-Type': cls.content_type})
+        requests.utils.add_dict_to_cookiejar(session.cookies, cookie_dict=cls.cookies)
+        return session
+
+    @staticmethod
+    def make_session2():
+        session = requests.session()
+        session.headers.update(HTTP_HEADERS)
+        print(session.headers.values())
+        return session
+
+
+if __name__ == '__main__':
+    http = HTTP()
+    http.make_session2()
